ec4ecef90a1c4124205bceaae9d8fcec7541dc42
ryananyangu <ryananyangu@gmail.com>
2020-05-26 16:57:27 +0300

    remove message bird processor template

--- src/main/java/com/custom/dialog/lab/externalServices/MessageBirdProcessor.java
+++ src/main/java/com/custom/dialog/lab/externalServices/MessageBirdProcessor.java
@@ -1,17 +1,17 @@
-package com.custom.dialog.lab.externalServices;
+// package com.custom.dialog.lab.externalServices;
 
-import java.util.List;
+// import java.util.List;
 
-import com.messagebird.MessageBirdClient;
-import com.messagebird.MessageBirdService;
-import com.messagebird.MessageBirdServiceImpl;
-// import com.messagebird.objects.conversations.Conversation;
-import com.messagebird.objects.conversations.ConversationContent;
+// import com.messagebird.MessageBirdClient;
+// import com.messagebird.MessageBirdService;
+// import com.messagebird.MessageBirdServiceImpl;
+// // import com.messagebird.objects.conversations.Conversation;
+// import com.messagebird.objects.conversations.ConversationContent;
 
-public class MessageBirdProcessor {
-    final MessageBirdService messageBirdService = new MessageBirdServiceImpl("test_gshuPaZoeEG6ovbc8M79w0QyM");
-    final MessageBirdClient messageBirdClient = new MessageBirdClient(messageBirdService,List.of(MessageBirdClient.Feature.ENABLE_CONVERSATION_API_WHATSAPP_SANDBOX));
-    ConversationContent conversationContent = new ConversationContent();
-    // conversationContent.setText("");      
-    // Conver
-}
+// public class MessageBirdProcessor {
+//     final MessageBirdService messageBirdService = new MessageBirdServiceImpl("test_gshuPaZoeEG6ovbc8M79w0QyM");
+//     final MessageBirdClient messageBirdClient = new MessageBirdClient(messageBirdService,List.of(MessageBirdClient.Feature.ENABLE_CONVERSATION_API_WHATSAPP_SANDBOX));
+//     ConversationContent conversationContent = new ConversationContent();
+//     // conversationContent.setText("");      
+//     // Conver
+// }
